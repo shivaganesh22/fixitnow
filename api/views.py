cee73678cba36ee24b3a325fc2cd42f7232546ab
SHIVA GANESH <rockstarshivaganesh@gmail.com>
2025-01-10 16:46:34 +0530

    remove

--- api/views.py
+++ api/views.py
@@ -81,19 +81,7 @@ class SignupView(APIView):
 
 class FCMv1Sender:
     PROJECT_ID = 'fixitnow-698b4'
-    SERVICE_ACCOUNT_JSON = {
-                        "type": "service_account",
-                        "project_id": "fixitnow-698b4",
-                        "private_key_id": "6eed46c69d6289dd8bd2387f1ef2b3af36ad1b65",
-                        "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQDDbw5GbKAyBDKC\nSUOSonuECiZtHqcOvdzBpJKIKfLwX0egS3EpvRtt2K+V76TWxvsRa2CGjsefMXiy\nCDKHrFfiGytcDEG+lDFMzKNa8K56MD6uE6X0IC/EADYH6emohoTAkNOdJ5lyUSwz\n6NaW3ZUv7WzwYVkUa2G/YL5rqB/aoscWT2rn/8VMnPdP7WhMxtqodvQIpACateAu\nNjoVaxpyKjePB1qE5EqA8BWeSHYsNvX7GYxl3Ex8uEy8J059wWSSZ63CK1+7x7LI\nMr/Tx5PLNplNvDOejGdyT7Yfti5oj0IwBdh3hhKJt9cnlubu5BedpoGE3hQz7VV2\nT1h60L73AgMBAAECggEAPVVDlh9uEPRXQ6MphUHf4Jrs308A37P/LXhwEjKUDxLf\n4DL88zvpeUpJJJV+VndBAQEBXIk/pbq5ZaoD7adNN2UOcQdZtK0+YZtRYu9o2mj8\nqtX4vYpCY1Ero8Upst1wmRx6hPYSBpnp9OqMOfGVaeKpzzDF644dsZlzeUbcpFjn\n58dW1FqFBr7iTQxYaF9xz5ZO04NhydDSfBOaOdrQKXGitRHX0CzHKEOkGLO+GSIp\ngMja0TMF4970BwTxLr4uJYp4BnbCXZ7XGcKBhQkdG2qNZE2kQFiKfBg4fmTCIwME\nmf9rg+cLUYSBJj+YmIguDXve8YnpwHRDmsLvEhjvDQKBgQDsEJnq//ymhVZwRPhP\nXWd6TS6F6EPWsYJVzFq3OVfSAh5IMU5IDZNVQNOraVl0dAFUAeSVA8F3tMJvBadC\nNzXwrenjr3UatBWSTpbcMFHYuvlorQ9O2BqdZsvbwSYz10ggcLU/aZTJXEup+4GF\nOfZNXCpxTkkGokpmOf6CXMoxqwKBgQDT8BEwE4JthRpj9+/VO+wfbWXWPXlFfAKd\nVXP/6D3Z02for5KWiGkrTnPGTYFyMMqDf4KHBmxrQh2PbDfKqOt+ZrCdeILRDVTn\nVJc8of1P6iMnUbfyHLKDyx5t0bk29/MjpsgpcJbNCc1NmwAUs/Ev3p7XNTiZwlQG\n8+S5s2fz5QKBgEvgVijT2RiJGSyC7rFL23vTHRYLfuqeKb98LNhhxRmKdsNLndbJ\nDkdDzAV3mjo1I0wmQ5umFmRspGZdEdLVvi+7Jsd5WRGZOqnJOvJRUa1xA8OihJ4a\nFgvrw7DB146oLtXGhGt6e0lxshxT6+CvrbxV2IqM2CoatgE/uM+cFZ17AoGATgvL\n6I5mq3omm8XEFhw4+eHJbLm6nPHr7JB5ZTXbAQou66ssKi8Vu5LqY45LKwf4q7Ab\nGyosZts0E4rgiMrn3eZnB7ZHRkDIkV+/Sd7Fb9ZjF6mqOYiD3LDCBeMDd9CbQve6\nIjiJ7/u6FOgNgZI6MUyj5dB9hXHgi6bpBdGhaFkCgYEA3CDZXEg7Hz7aDJnrsn+q\nyGbkyH03xIaZkW0OvOziVsxvPaT39mFUsPR9nfcqwhDFAVgbsOZMey2HtmvRiwCu\nWD8hHrcCgO7FQSjTJY+zdQMqZuzp5/Mi+CMIX08TBiDSDU0bq9+mLd4bf7HrSrRe\nL4t8okHAtCo6gvZFUq/gu18=\n-----END PRIVATE KEY-----\n",
-                        "client_email": "fixitnow-service-new@fixitnow-698b4.iam.gserviceaccount.com",
-                        "client_id": "101186740008532573154",
-                        "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-                        "token_uri": "https://oauth2.googleapis.com/token",
-                        "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-                        "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/fixitnow-service-new%40fixitnow-698b4.iam.gserviceaccount.com",
-                        "universe_domain": "googleapis.com"
-        }
+    
     def __init__(self):
         
         self.credentials = service_account.Credentials.from_service_account_info(
--- fixitnow/settings.py
+++ fixitnow/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^7ds0o+cnr3!0me!&u+5ljnkd^l#u76wd=n!3ile0$nl99h655'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 import os
 ALLOWED_HOSTS = ['*']
 
